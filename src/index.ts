924d1c2da52c252d4dd653cc1474c8b3ef7a2623
Technion <technion@lolware.net>
2021-11-25 21:09:41 +1100

    Updated fetch checks

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 
 const fetch_with_checks = (resource: string, init: RequestInit = {}): Promise<Response> => {
-
-    let myHeaders = new fetch.Headers();
-
     let checked_url = new URL(resource); // Will throw on its own on an invalid url
 
     let checked_init: RequestInit = {}
     if (checked_url.username) {
         throw new Error("Provided Username")
+    }
+    if (! ["https:", "http:"].includes(checked_url.protocol) ) {
+        throw new Error("Invalid scheme provided")
     }
     if (init.headers) {
         console.log(init.headers);
