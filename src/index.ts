1cccbad6df27b7e7a510a500a5a9987fd9fad490
Technion <technion@lolware.net>
2021-11-26 19:47:48 +1100

    Fixed test suite by hard locking old node-fetch

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
+const fetch_with_checks = (
+  resource: string,
+  init: RequestInit = {}
+): Promise<Response> => {
+  let checked_url = new URL(resource); // Will throw on its own on an invalid url
 
-const fetch_with_checks = (resource: string, init: RequestInit = {}): Promise<Response> => {
-    let checked_url = new URL(resource); // Will throw on its own on an invalid url
-
-    let checked_init: RequestInit = {}
-    if (checked_url.username) {
-        throw new Error("Provided Username")
-    }
-    if (! ["https:", "http:"].includes(checked_url.protocol) ) {
-        throw new Error("Invalid scheme provided")
-    }
-    if (init.headers) {
-        console.log(init.headers);
-        checked_init.headers = new Headers(init.headers); // Will throw on its own on an invalid header
-    }
-    return fetch(checked_url.href, checked_init);
-}
+  let checked_init: RequestInit = {};
+  if (checked_url.username) {
+    throw new Error("Provided Username");
+  }
+  if (!["https:", "http:"].includes(checked_url.protocol)) {
+    throw new Error("Invalid scheme provided");
+  }
+  if (init.headers) {
+    checked_init.headers = new Headers(init.headers); // Will throw on its own on an invalid header
+  }
+  return fetch(checked_url.href, checked_init);
+};
 
 export default fetch_with_checks;
--- test/suite.ts
+++ test/suite.ts
@@ -1,32 +1,40 @@
-import  { assert } from "chai";
+import { assert } from "chai";
 import fetch, { Headers } from "node-fetch";
 import fetch_with_checks from "../src/index";
 
 global.fetch = fetch;
+global.Headers = Headers;
 
-describe('URL Checks', () => {
-    it('Should reject dumb URL', () => {
-       assert.throw(() => {fetch_with_checks("holy kek") }, "Invalid URL");
-    })
+describe("URL Checks", () => {
+  it("Should reject dumb URL", () => {
+    assert.throw(() => {
+      fetch_with_checks("holy kek");
+    }, "Invalid URL");
+  });
 
-    it('Should reject an invalid header', () => {
-      const headers = {
-            "C ontent-Type": "text/xml",
-            "Breaking-Bad": "<3"
-        };
-        assert.throw(() => {fetch_with_checks("https://example.com/", { headers }) }, "Test");
-     })
-     it('Should reject an invalid header', () => {
-        const headers = [
-            ["Content-Type", "text/html", "extra"],
-            ["Accept"],
-        ];
-        assert.throw(() => {fetch_with_checks("https://example.com/", { headers }) }, "Test");
-    })
-    it('Should an invalid scheme', () => {
-        assert.throw(() => {fetch_with_checks("file://localhost") }, "Invalid scheme");
-      })
-    it('Should reject a username URL', () => {
-        assert.throw(() => {fetch_with_checks("https://user:password@example.com/") }, "Provided Username");
-      })
+  it("Should reject an invalid header", () => {
+    const headers = {
+      "C ontent-Type": "text/xml",
+      "Breaking-Bad": "<3",
+    };
+    assert.throw(() => {
+      fetch_with_checks("https://example.com/", { headers });
+    }, "Header name must be a valid");
+  });
+  it("Should reject an invalid header", () => {
+    const headers = [["Content-Type", "text/html", "extra"], ["Accept"]];
+    assert.throw(() => {
+      fetch_with_checks("https://example.com/", { headers });
+    }, "Each header pair");
+  });
+  it("Should an invalid scheme", () => {
+    assert.throw(() => {
+      fetch_with_checks("file://localhost");
+    }, "Invalid scheme");
+  });
+  it("Should reject a username URL", () => {
+    assert.throw(() => {
+      fetch_with_checks("https://user:password@example.com/");
+    }, "Provided Username");
+  });
 });
